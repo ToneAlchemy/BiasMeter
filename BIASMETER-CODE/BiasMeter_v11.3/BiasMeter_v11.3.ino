3b50dadb1414124751b3e4c77bb809d3a097cd62
Charlie Isaac <34847969+TheSupremeUltimate@users.noreply.github.com>
2026-06-18 13:06:35 +1000

    Update header for version 11.4.1 and license change
    Updated license information and version number in the header.

--- BIASMETER-CODE/BiasMeter_v11.3/BiasMeter_v11.3.ino
+++ BIASMETER-CODE/BiasMeter_v11.3/BiasMeter_v11.3.ino
@@ -1,14 +1,23 @@
-/*
- * Arduino Tube Amp Bias Meter (V11.3 Platinum)
+ /** Arduino Tube Amp Bias Meter (V11.4.1 WTD Platinum - GOLD MASTER)
  * * Copyright (C) 2024-2026 Charlie Isaac (ToneAlchemy.com)
- * * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  * * ======================================================================
  * ATTRIBUTION & HISTORY
  * This project is an evolutionary adaptation based on:
@@ -16,9 +25,8 @@
  * Original License: Creative Commons Attribution 4.0 International (CC BY 4.0).
  * 2. "ArduinoBiasMeter" by John Wagner.
  * Original License: Creative Commons Attribution 4.0 International (CC BY 4.0).
- * * Per the terms of the CC BY 4.0 license, this adaptation is released 
- * under the GNU General Public License v3.0 (GPLv3) to ensure all future 
- * derivatives remain open-source.
+ * * Per the terms of the CC BY 4.0 license, this adaptation is released
+ * under the MIT license to ensure all future derivatives remain open-source.
  * ======================================================================
  */
 
